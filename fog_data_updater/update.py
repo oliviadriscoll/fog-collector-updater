a7deb186ec936ba7cbfc54fbbac5d08d6495ca5f
Olivia Driscoll <tkdka088@gmail.com>
2022-08-23 00:20:02 -0700

    Use Sheets batch API

--- fog_data_updater/update.py
+++ fog_data_updater/update.py
@@ -1,6 +1,8 @@
 import argparse
 import datetime
 import json
+import logging
+import os
 import pprint
 
 
@@ -46,7 +48,35 @@ def query_zentra(token):
     return result
 
 
+"""
+[10, 20, 30] -> [20, 40, 60]
+
+result = []
+for x in arr:
+    result.append(x * 2)
+
+result = [x * 2 for x in arr]
+"""
+
+
+def cell_update_from_data(data):
+    return [
+        {"values": [{"userEnteredValue": {"numberValue": value}} for value in row]}
+        for row in data
+    ]
+
+    result = []
+    for row in data:
+        row_result = []
+        for value in row:
+            row_result.append({"userEnteredValue": {"numberValue": value}})
+        result.append({"values": row_result})
+    return result
+
+
 def main():
+    logging.basicConfig(level=os.environ.get("PYTHON_LOG", "INFO"))
+
     parser = argparse.ArgumentParser()
     parser.add_argument("secret_file")
     args = parser.parse_args()
@@ -56,18 +86,58 @@ def main():
     creds = creds.with_scopes(scopes)
     service = googleapiclient.discovery.build("sheets", "v4", credentials=creds)
 
-    range_name = "Sheet1!A1:D2"
-    values = [
-        ["a1", "b1", "c1", 123],
-        ["a2", "b2", "c2", 456],
-    ]
-    data = {"values": values}
-    service.spreadsheets().values().update(
-        spreadsheetId=SPREADSHEET_ID,
-        body=data,
-        range=range_name,
-        valueInputOption="USER_ENTERED",
-    ).execute()
+    metadata = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+    sheets = metadata.get("sheets", "")
+    sheet_id = sheets[0].get("properties", {}).get("sheetId", 0)
+
+    # https://googleapis.github.io/google-api-python-client/docs/dyn/sheets_v4.spreadsheets.html
+    # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/batchUpdate
+    # https://stackoverflow.com/questions/38245714/get-list-of-sheets-and-latest-sheet-in-google-spreadsheet-api-v4-in-python
+    body = {
+        "requests": [
+            {
+                "deleteRange": {
+                    "range": {
+                        "sheetId": sheet_id,
+                        "startRowIndex": 0,
+                        "startColumnIndex": 0,
+                    },
+                    "shiftDimension": "ROWS",
+                },
+            },
+            {
+                "appendCells": {
+                    "sheetId": sheet_id,
+                    "rows": [
+                        {
+                            "values": [
+                                {"userEnteredValue": {"numberValue": 10}},
+                                {"userEnteredValue": {"numberValue": 20}},
+                            ]
+                        },
+                        {
+                            "values": [
+                                {"userEnteredValue": {"numberValue": 30}},
+                                {"userEnteredValue": {"numberValue": 40}},
+                            ]
+                        },
+                    ],
+                    "fields": "userEnteredValue",
+                }
+            },
+        ]
+    }
+    try:
+        response = (
+            service.spreadsheets()
+            .batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body)
+            .execute()
+        )
+    except Exception as e:
+        print("Caught the following exception:")
+        print(e)
+
+    logging.info(pprint.pformat(response))
 
 
 if __name__ == "__main__":
